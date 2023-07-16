ba3f0968f59c34231c7815ae07f2f16faa29fa17
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-07-16 14:51:21 +0300

    feat: add url strategy support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:md_crossword/src/app.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
+  usePathUrlStrategy();
   runApp(const Crossword());
 }
